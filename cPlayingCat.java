d89f81fb7d6630a38d0954aa172c4147655243a0
AdeiTeck-star <nthabisengadelaidethema20@gmail.com>
2023-05-28 23:46:34 +0200

    the playing cat

---
+++ cPlayingCat.java
@@ -0,0 +1,55 @@
+/**
+ * The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive).
+ * Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+ * <p>
+ * <p>
+ * <p>
+ * Write a method isCatPlaying that has 2 parameters. Method needs to return true if the cat is playing, otherwise return false
+ * <p>
+ * 1st parameter should be of type boolean and be named summer it represents if it is summer.
+ * 2nd parameter represents the temperature and is of type int with the name temperature.
+ */
+
+package JavaProblems;
+
+public class cPlayingCat {
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(false,35));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+
+        if(temperature < 25){
+            return false;
+        }
+
+         if(summer == true){
+
+            if(temperature>=25 && temperature<= 45){
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+        else if(!summer){
+            if(temperature>=25 && temperature<= 35){
+                return true;
+            }
+            else {
+                return false;
+
+            }
+        }
+
+            return false;
+
+
+    }
+
+}
